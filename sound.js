ce3b479e2d12a98b914fc94af71f92a1acb259be
mimizukmsk <mizuku3k@gmail.com>
2018-10-11 11:03:54 +0900

    3, Oscillator

--- sound.js
+++ sound.js
@@ -1,12 +1,35 @@
 window.AudioContext = window.webkitAudioContext||window.AudioContext;
 const audioctx = new AudioContext();
 
+let play = 0;
 const osc = audioctx.createOscillator();
-osc.connect(audioctx.destination);
+const gain = audioctx.createGain();
+osc.connect(gain);
+gain.connect(audioctx.destination);
 
-document.getElementById('play').onclick = function() {
-  osc.start();
+function Setup() {
+  if(play == 0) {
+    osc.start();
+    play = 1;
+  }
+  let type = document.getElementById("type").value;
+  let freq = parseFloat(document.getElementById("freq").value);
+  let level = parseFloat(document.getElementById("level").value);
+
+  osc.type = type;
+  osc.frequency.value = freq;
+  gain.gain.value = level;
 }
-document.getElementById('stop').onclick = function() {
+
+const setup = document.getElementsByClassName("setup");
+for (let i = 0; i < 3; i++) {
+  setup[i].onchange = function() {
+    Setup();
+  }
+}
+setup[3].onclick = function() {
+  Setup();
+}
+document.getElementById("stop").onclick = function() {
   osc.stop();
 }
